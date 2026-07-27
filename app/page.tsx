2fa1e34b94f037aed4ec5b9d04f5378c92589081
Agbadudu Oghenerukevwe <90446020+Dev-rukky@users.noreply.github.com>
2026-07-27 18:29:15 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import { getLandingData } from "@/lib/nara/bayse.function";
 import Navbar from "@/components/nara/navbar";
 import { Hero } from "@/components/nara/hero";
-// import { MarketTicker } from "@/components/nara/marketTicker";
-import { ProductBento } from "@/components/nara/products"; // Note: verify this path matches your file!
-// import { SignalTimeline } from "@/components/nara/signalTimeline";
-// import { SocialProof } from "@/components/nara/socialProof";
-// import { NewsletterCTA } from "@/components/nara/newsletter";
+import { MarketTicker } from "@/components/nara/marketTicker";
+import { ProductBento } from "@/components/nara/products";
+import { SignalTimeline } from "@/components/nara/signalTimeline";
+import { SocialProof } from "@/components/nara/socialProof";
+import { NewsletterCTA } from "@/components/nara/newsletter";
 import { PricingSection } from "@/components/nara/pricingSection";
 import { Footer } from "@/components/nara/footer";
 
@@ -16,20 +16,11 @@ export default async function HomePage() {
     <main className="min-h-screen bg-nara-black text-nara-text selection:bg-nara-amber selection:text-nara-black">
       <Navbar />
       <Hero initialData={initialLandingData} />
-      
-      {/* 
-        Temporarily disabled until we convert them to Next.js:
-        <MarketTicker initialData={initialLandingData} /> 
-      */}
-      
+      <MarketTicker initialData={initialLandingData} /> 
       <ProductBento initialData={initialLandingData} />
-      
-      {/* 
-        <SignalTimeline />
-        <SocialProof initialData={initialLandingData} />
-        <NewsletterCTA /> 
-      */}
-      
+      <SignalTimeline />
+      <SocialProof initialData={initialLandingData} />
+      <NewsletterCTA /> 
       <PricingSection />
       <Footer />
     </main>
